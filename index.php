247980a63cf73809d6bb85dc6ab51510b3cf8ac6
ChandraAhmadPudoli <2210631170114@student.unsika.ac.id>
2024-05-27 11:00:40 +0700

    progress kedua

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aplikasi Perpustakaan</title>
+    <title>Aplikasi Perpustakaan V.2</title>
 </head>
 <body>
     <h1>Aplikasi Perpustakaan</h1>
